8125bd7de8396eb1741c8f3de7b6bee966757969
Arihant416 <arihantjain416@gmail.com>
2020-05-14 08:36:39 +0530

    Use routes

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) =>
     res.json({ msg: 'Welcome to Contact Keeper API :)' })
 );
 
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server listening on Port ${PORT}`));
