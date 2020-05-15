c132b42d9ed9e6afee7b33e14309ce70ce27593b
Arihant416 <arihantjain416@gmail.com>
2020-05-15 21:21:15 +0530

    Contact Filter Form and State

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,9 +1,23 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ContactContext from '../../context/contacts/contactContext';
 
 const ContactForm = () => {
     const contactContext = useContext(ContactContext);
 
+    const { addContact, updateContact, clearCurrent, current } = contactContext;
+
+    useEffect(() => {
+        if (current !== null) {
+            setContact(current);
+        } else {
+            setContact({
+                name: '',
+                email: '',
+                phone: '',
+                type: 'personal',
+            });
+        }
+    }, [contactContext, current]);
     const [contact, setContact] = useState({
         name: '',
         email: '',
@@ -18,18 +32,28 @@ const ContactForm = () => {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        contactContext.addContact(contact);
+        if (current === null) {
+            addContact(contact);
+        } else {
+            updateContact(contact);
+        }
         setContact({
             name: '',
             email: '',
             phone: '',
             type: 'personal',
         });
+        clearAll();
+    };
+    const clearAll = () => {
+        clearCurrent();
     };
 
     return (
         <form onSubmit={onSubmit}>
-            <h2 className='text-primary'>Add Contact</h2>
+            <h2 className='text-primary'>
+                {current ? 'Edit Contact' : 'Add Contact'}
+            </h2>
             <input
                 type='text'
                 placeholder='Name'
@@ -71,10 +95,17 @@ const ContactForm = () => {
             <div>
                 <input
                     type='submit'
-                    value='Add Contact'
+                    value={current ? 'Update Contact' : 'Add Contact'}
                     className='btn btn-primary btn-block'
                 />
             </div>
+            {current && (
+                <div>
+                    <button class='btn btn-light btn-block' onClick={clearAll}>
+                        Clear
+                    </button>
+                </div>
+            )}
         </form>
     );
 };
