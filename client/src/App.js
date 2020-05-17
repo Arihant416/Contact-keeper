fe300694277ba371da4b4fce7fb3950e0e654187
Arihant416 <arihantjain416@gmail.com>
2020-05-17 19:36:13 +0530

    Load User & Set Token

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,12 @@ import Alerts from './components/layouts/Alerts';
 import ContactState from './context/contacts/ContactStates';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
+
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
 
 const App = () => {
     return (
